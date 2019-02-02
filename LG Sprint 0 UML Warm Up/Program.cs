70da46171dbb66e8031d9576a8b0c0449d60c0f9
luke-goldfain <awsumluke22@gmail.com>
2019-02-02 16:40:20 -0600

    Add project files.

---
+++ LG Sprint 0 UML Warm Up/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LG_Sprint_0_UML_Warm_Up
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
