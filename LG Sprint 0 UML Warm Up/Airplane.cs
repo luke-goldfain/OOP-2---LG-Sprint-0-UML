21cdbf6fb209b413499ac938c65ab5954707e8ab
luke-goldfain <awsumluke22@gmail.com>
2019-02-09 12:31:18 -0600

    actual commit

---
+++ LG Sprint 0 UML Warm Up/Airplane.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LGSprint0UML
+{
+    class Airplane : AerialVehicle
+    {
+        public Airplane()
+        {
+            this.defaultFlyHeight = 1000;
+            this.MaxAltitude = 41000;
+        }
+    }
+}
---
+++ LG Sprint 0 UML Warm Up/Engine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LGSprint0UML
+{
+    internal class Engine
+    {
+        public bool IsStarted = false;
+
+        public Engine()
+        {
+
+        }
+
+        public string About()
+        {
+            string aboutStr = "";
+
+            return aboutStr;
+        }
+
+        public void Start()
+        {
+            this.IsStarted = true;
+        }
+
+        public void Stop()
+        {
+            this.IsStarted = false;
+        }
+    }
+}
